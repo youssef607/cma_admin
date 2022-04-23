e18b12287215295dd6d18a49525e2217deeaca7c
youssef607 <yg77848@gmail.com>
2022-04-23 22:52:32 +0000

    st

--- lib/presentation/base/baseviewmodel.dart
+++ lib/presentation/base/baseviewmodel.dart
@@ -1,33 +1,34 @@
-// import 'dart:async';
+import 'dart:async';
 
-// import 'package:rxdart/rxdart.dart';
+import 'package:cma_admin/presentation/common/state_renderer/state_render_impl.dart';
+import 'package:rxdart/rxdart.dart';
 
-// abstract class BaseViewModel extends BaseViewModelInputs
-//     with BaseViewModelOutputs {
-//   StreamController _inputStateStreamController = BehaviorSubject<FlowState>();
+abstract class BaseViewModel extends BaseViewModelInputs
+    with BaseViewModelOutputs {
+  StreamController _inputStateStreamController = BehaviorSubject<FlowState>();
 
-//   @override
-//   Sink get inputState => _inputStateStreamController.sink;
+  @override
+  Sink get inputState => _inputStateStreamController.sink;
 
-//   @override
-//   Stream<FlowState> get outputState =>
-//       _inputStateStreamController.stream.map((flowState) => flowState);
+  @override
+  Stream<FlowState> get outputState =>
+      _inputStateStreamController.stream.map((flowState) => flowState);
 
-//   @override
-//   void dispose() {
-//     _inputStateStreamController.close();
-//   }
+  @override
+  void dispose() {
+    _inputStateStreamController.close();
+  }
 
-// // shared variables and functions that will be used through any view model.
-// }
+// shared variables and functions that will be used through any view model.
+}
 
-// abstract class BaseViewModelInputs {
-//   void start(); // will be called while init. of view model
-//   void dispose(); // will be called when viewmodel dies.
+abstract class BaseViewModelInputs {
+  void start(); // will be called while init. of view model
+  void dispose(); // will be called when viewmodel dies.
 
-//   Sink get inputState;
-// }
+  Sink get inputState;
+}
 
-// abstract class BaseViewModelOutputs {
-//   Stream<FlowState> get outputState;
-// }
+abstract class BaseViewModelOutputs {
+  Stream<FlowState> get outputState;
+}
