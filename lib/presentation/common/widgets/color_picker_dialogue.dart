3f35def4b9bb828125fda3e0966d1dbed60abff3
youssef607 <yg77848@gmail.com>
2022-05-17 21:03:29 +0000

    dialoue

--- lib/presentation/common/widgets/color_picker_dialogue.dart
+++ lib/presentation/common/widgets/color_picker_dialogue.dart
@@ -6,21 +6,12 @@ void showAlert(
     BuildContext context, Color color, final Function(Color) onchanged) {
   showDialog(
     context: context,
-    builder: (context) => AlertDialog(
-      title: Center(child: Text(AppStrings.pickColor)),
-      content: ColorPicker(
+    builder: (context) => Dialog(
+      child: ColorPicker(
         showLabel: false,
         pickerColor: color,
         onColorChanged: onchanged,
       ),
-      actions: [
-        ElevatedButton(
-          child: const Text(AppStrings.goIt),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-      ],
     ),
   );
 }
